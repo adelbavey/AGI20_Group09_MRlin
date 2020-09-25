49929e510a8815efdb4d4e5ed39d0ab2b62f1981
stjerndal <stjerndale@gmail.com>
2020-09-25 16:46:14 +0200

    Add collision trigger feature to ProjectileManager and new VFX graphs

--- MRlin/Assets/Original VFX/ProjectileManager.cs
+++ MRlin/Assets/Original VFX/ProjectileManager.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
 
 public class ProjectileManager : MonoBehaviour
 {
 
     public float speed = 15f;
+    public GameObject explosionEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,8 +23,19 @@ public class ProjectileManager : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        speed = 0;
+        Explode();
+        //Invoke("Explode", 3.0f);
+    }
+    private void Explode()
+    {
+        GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        GameObject.Destroy(gameObject);
     }
 }
