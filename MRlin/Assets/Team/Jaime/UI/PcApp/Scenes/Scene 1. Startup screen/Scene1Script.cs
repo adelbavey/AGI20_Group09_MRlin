498f4f72727e97e94244f90a8208306627c503bf
Jaime <jaime.vallejob@alumnos.upm.es>
2020-10-23 00:02:13 +0200

    Start working on the UI. First scene almost done
    Most part of the time struggling with API's issues and learning how to use Unity, but step by step I'm managing to do some stuff. Of course you can comment and give advice or suggestions :)

---
+++ MRlin/Assets/Team/Jaime/UI/PcApp/Scenes/Scene 1. Startup screen/Scene1Script.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Scene1Script : MonoBehaviour
+{
+    public float interval;
+    public GameObject spaceText;
+    public GameObject particles;
+
+    private ParticleSystem particlesSys;
+    private float alphaValue;
+    private bool goingDown;
+    private TextMeshProUGUI text;
+    private Transform pos;
+
+    private void Awake()
+    {
+        particlesSys = particles.GetComponent<ParticleSystem>();
+        text = spaceText.GetComponent<TextMeshProUGUI>();
+        if (spaceText == null) Debug.Log("Error. No spaceText attached");
+        if (text == null) Debug.Log("Error. No text attached");
+        alphaValue = 255;
+        goingDown = true;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles.SetActive(true);
+        InvokeRepeating("FlashText", 0, interval);
+    }
+
+    void FlashText()
+    {
+        if (goingDown)
+        {
+            if (alphaValue == 60)
+            {
+                alphaValue+=15;
+                goingDown = false;
+            }
+            alphaValue-=15;
+        }
+        else
+        {
+            if (alphaValue == 255)
+            {
+                alphaValue-=15;
+                goingDown = true;
+            }
+            alphaValue+=15;
+        }
+        if(text == null)
+        {
+            Debug.Log("Error. Not text attached");
+        }
+       
+        text.alpha = alphaValue/255;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
