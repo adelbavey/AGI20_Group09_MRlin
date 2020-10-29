b6a1a02d55721d22999a05b5645b38984c2372aa
omega <adel.bavey@gmail.com>
2020-10-29 13:46:23 +0100

    beginning network test

---
+++ MRlin/Assets/Team/Adel/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+using UnityEngine.UI;
+
+public class PlayerScript : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMesh isPC = GetComponentInChildren<TextMesh>();
+        //Check if the device running this is a desktop
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            //m_DeviceType = "Desktop";
+            isPC.text = "Desktop";
+        }
+
+        //Check if the device running this is a handheld
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            isPC.text = "Handheld";
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
