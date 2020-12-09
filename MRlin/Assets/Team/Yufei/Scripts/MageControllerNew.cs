46dd43107c0f08c56499d6494ea00edd0e522287
Yuphay <yuphayzhang@gmail.com>
2020-12-09 18:40:04 +0100

    VFX finished. Final Gameplay in progress.

--- MRlin/Assets/Team/Yufei/Scripts/MageControllerNew.cs
+++ MRlin/Assets/Team/Yufei/Scripts/MageControllerNew.cs
@@ -6,16 +6,20 @@ using UnityEngine.VFX;
 
 public class MageControllerNew : MonoBehaviour
 {
-    public bool phoneInteraction = true;
+    [Tooltip("1: Player 1; 2: Player 2")]
+    public int playerIndex = 1;
+    public bool gyroInteraction = false;
     public Transform targetTransform;
     public Transform wandTransform;
-    public Transform wandMagicTransform;
-    //public Transform runeTransform;
-    //public AudioSource leftRune;
-    //public AudioSource midRune;
-    //public AudioSource rightRune;
+    [SerializeField]
+    private Transform wandMagicTransform;
+
+    public List<Transform> ourIslandTransforms;
+    public List<Transform> oppoIslandTransforms;
+    public List<Transform> islandHighlightTransforms;
 
     public LayerMask mouseAimMask;
+    public LayerMask transparentLayer;
     public Transform gyroRayObject;
 
     public int currentHealth;
@@ -27,6 +31,7 @@ public class MageControllerNew : MonoBehaviour
         public Transform wandTransform;
         public int spell;
     }
+    public int gestureCode;
 
     //private Animator animator;
     private Rigidbody rBody;
@@ -47,19 +52,34 @@ public class MageControllerNew : MonoBehaviour
     private VisualEffectAsset fireBigVFX2;
     [SerializeField]
     private VisualEffectAsset fireBigVFX3;
+
+    [SerializeField]
+    private Transform headFireSmall;
+    [SerializeField]
+    private VisualEffectAsset fireSmallVFX1;
+    [SerializeField]
+    private VisualEffectAsset fireSmallVFX2;
+    [SerializeField]
+    private VisualEffectAsset fireSmallVFX3;
+
     [SerializeField]
     private Transform headGlow;
     [SerializeField]
     private VisualEffectAsset headGlowVFX1;
     [SerializeField]
     private VisualEffectAsset headGlowVFX2;
+
     [SerializeField]
-    private VisualEffectAsset headGlowVFX3;
+    private Transform headSparks;
+    [SerializeField]
+    private VisualEffectAsset headSparksVFX1;
+    [SerializeField]
+    private VisualEffectAsset headSparksVFX2;
 
     private ParticleSystem wandMagicParticle;
     private AudioSource wandMagicSound;
     private TrailRenderer wandMagicTrail;
-    private enum gamePhase { Idle, Spelling, Casting };
+    private enum gamePhase { Idle, Spelling, Selecting, Shooting };
     private gamePhase currentPhase;
 
     /* Void: No successful spell.
@@ -72,12 +92,36 @@ public class MageControllerNew : MonoBehaviour
     private Camera mainCamera;
     private Vector3 targetVector;
 
+    private enum magePos { Left, Middle, Right };
+    private magePos currentPos;
+    private Vector3 oldMagePosition;
+
+    private Transform currentTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        //animator = GetComponent<Animator>();
+        if (playerIndex == 1)
+        {
+            headGlow.GetComponent<VisualEffect>().visualEffectAsset = headGlowVFX1;
+            headSparks.GetComponent<VisualEffect>().visualEffectAsset = headSparksVFX1;
+        }
+        else if (playerIndex == 2)
+        {
+            headGlow.GetComponent<VisualEffect>().visualEffectAsset = headGlowVFX2;
+            headSparks.GetComponent<VisualEffect>().visualEffectAsset = headSparksVFX2;
+        }
+        else
+        {
+            Debug.LogError("Player index out of bound.");
+        }
+        headFireBig.GetComponent<VisualEffect>().Stop();
+        headFireSmall.GetComponent<VisualEffect>().Stop();
+
         rBody = GetComponent<Rigidbody>();
         currentSpell = spellElement.Void;
+        gestureCode = 0;
+        currentPos = magePos.Left;
 
         wandMagicParticle = wandMagicTransform.GetComponent<ParticleSystem>();
         wandMagicSound = wandMagicTransform.GetComponent<AudioSource>();
@@ -92,12 +136,23 @@ public class MageControllerNew : MonoBehaviour
 
         currentHealth = 100;
         healthBar.setHealth(currentHealth);
+
+        for (int i = 0; i < 3; i++)
+        {
+            islandHighlightTransforms[i].GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        Physics.IgnoreLayerCollision(0, 8);
+        Physics.IgnoreLayerCollision(0, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (phoneInteraction)
+        mainCamera.transform.rotation = Quaternion.LookRotation(
+            Vector3.Normalize(oppoIslandTransforms[1].position - mainCamera.transform.position));
+
+        if (gyroInteraction)
         {
             Ray newRay = new Ray(mouseRayObject.position, mouseRayObject.forward);
 
@@ -107,24 +162,48 @@ public class MageControllerNew : MonoBehaviour
                 targetTransform.position = hit.point;
             }
 
-            if (Input.GetMouseButtonDown(0))
+            // Both mouse and phone screen tap
+            StateMachineTransition();
+
+            //TODO: Camera-controlled movement 
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                StartSpellingPhase(true);
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                StartSpellingPhase(false);
-                // Check if the subscriber is null and pass in spell element info
-                OnCastingStarts?.Invoke(this, new OnCastingStartsEventArgs
+                if (currentPos == magePos.Middle)
                 {
-                    wandTransform = this.wandTransform,
-                    spell = (int)currentSpell
-                });
-                currentPhase = gamePhase.Casting;
-                Invoke("Damage", 1.0f);
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[0].position + (oldMagePosition - ourIslandTransforms[1].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Left;
+                }
+                else if (currentPos == magePos.Right)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[1].position + (oldMagePosition - ourIslandTransforms[2].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Middle;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (currentPos == magePos.Middle)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[2].position + (oldMagePosition - ourIslandTransforms[1].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Right;
+                }
+                else if (currentPos == magePos.Left)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[1].position + (oldMagePosition - ourIslandTransforms[0].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Middle;
+                }
             }
         }
-        else // Keyboard and mouse interaction
+
+        // Keyboard and mouse interaction
+        else
         {
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -135,36 +214,77 @@ public class MageControllerNew : MonoBehaviour
                 mouseRayObject.rotation = Quaternion.LookRotation(ray.direction);
             }
 
+            // Both mouse and phone screen tap
+            StateMachineTransition();
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (currentPos == magePos.Middle)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[0].position + (oldMagePosition - ourIslandTransforms[1].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Left;
+                }
+                else if (currentPos == magePos.Right)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[1].position + (oldMagePosition - ourIslandTransforms[2].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Middle;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (currentPos == magePos.Middle)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[2].position + (oldMagePosition - ourIslandTransforms[1].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Right;
+                }
+                else if (currentPos == magePos.Left)
+                {
+                    oldMagePosition = transform.position;
+                    transform.position = ourIslandTransforms[1].position + (oldMagePosition - ourIslandTransforms[0].position);
+                    mainCamera.transform.position += transform.position - oldMagePosition;
+                    currentPos = magePos.Middle;
+                }
+            }
+        }
+    }
+
+    private void StateMachineTransition()
+    {
+        if (currentPhase == gamePhase.Idle)
+        {
             if (Input.GetMouseButtonDown(0))
             {
                 StartSpellingPhase(true);
             }
-            else if (Input.GetMouseButtonUp(0))
+        }
+        else if (currentPhase == gamePhase.Spelling)
+        {
+            if (Input.GetMouseButtonUp(0))
             {
                 StartSpellingPhase(false);
-                // Check if the subscriber is null and pass in spell element info
-                OnCastingStarts?.Invoke(this, new OnCastingStartsEventArgs
-                {
-                    wandTransform = this.wandTransform,
-                    spell = (int)currentSpell
-                });
-                currentPhase = gamePhase.Casting;
-                Invoke("Damage", 1.0f);
+                StartSelectingPhase(true);
             }
-
-            if (currentPhase == gamePhase.Spelling)
+        }
+        else if (currentPhase == gamePhase.Selecting)
+        {
+            SelectingPhase();
+            if (Input.GetMouseButtonDown(0))
             {
-                SpellingPhase();
+                StartSelectingPhase(false);
+                StartShootingPhase(true);
             }
-
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        }
+        else if (currentPhase == gamePhase.Shooting)
+        {
+            if (Input.GetMouseButtonUp(0))
             {
-                
-            }
-
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-
+                StartShootingPhase(false);
             }
         }
     }
@@ -177,6 +297,8 @@ public class MageControllerNew : MonoBehaviour
             wandMagicParticle.Play();
             wandMagicSound.Play();
             wandMagicTrail.enabled = true;
+
+            transform.GetComponent<PlayerInputMRlin>().gestureRecognition = true;
         }
         else
         {
@@ -185,30 +307,93 @@ public class MageControllerNew : MonoBehaviour
             wandMagicSound.Stop();
             wandMagicTrail.enabled = false;
             wandMagicTrail.Clear();
+
+            transform.GetComponent<PlayerInputMRlin>().gestureRecognition = false;
+            transform.GetComponent<PlayerInputMRlin>().patternRecognition();
         }
     }
 
-    private void SpellingPhase()
+    private void StartSelectingPhase(bool b)
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (b)
         {
-            headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX1;
-            headGlow.GetComponent<VisualEffect>().visualEffectAsset = headGlowVFX1;
-            currentSpell = spellElement.Ice;
+            currentPhase = gamePhase.Selecting;
+
+            // Handle the result of the spelling phase
+            switch (gestureCode)
+            {
+                case 0:
+                    currentSpell = spellElement.Void;
+                    break;
+                case 1:
+                    currentSpell = spellElement.Ice;
+                    headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX1;
+                    headFireSmall.GetComponent<VisualEffect>().visualEffectAsset = fireSmallVFX1;
+                    break;
+                case 2:
+                    currentSpell = spellElement.Storm;
+                    headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX2;
+                    headFireSmall.GetComponent<VisualEffect>().visualEffectAsset = fireSmallVFX2;
+                    break;
+                case 3:
+                    currentSpell = spellElement.Fire;
+                    headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX3;
+                    headFireSmall.GetComponent<VisualEffect>().visualEffectAsset = fireSmallVFX3;
+                    break;
+                case 4:
+                    currentSpell = spellElement.Shield;
+                    break;
+                default:
+                    currentSpell = spellElement.Void;
+                    break;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.W))
+        else
         {
-            headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX2;
-            headGlow.GetComponent<VisualEffect>().visualEffectAsset = headGlowVFX2;
-            currentSpell = spellElement.Storm;
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            headFireBig.GetComponent<VisualEffect>().visualEffectAsset = fireBigVFX3;
-            headGlow.GetComponent<VisualEffect>().visualEffectAsset = headGlowVFX3;
-            currentSpell = spellElement.Fire;
+            currentPhase = gamePhase.Idle;
+            for (int i = 0; i < 3; i++)
+            {
+                islandHighlightTransforms[i].GetComponent<MeshRenderer>().enabled = false;
+            }
         }
     }
+
+    private void StartShootingPhase(bool b)
+    {
+        if (b)
+        {
+            currentPhase = gamePhase.Shooting;
+            // Check if the subscriber is null and pass in spell element info
+            OnCastingStarts?.Invoke(this, new OnCastingStartsEventArgs
+            {
+                wandTransform = this.wandTransform,
+                spell = (int)currentSpell
+            });
+            Invoke("Damage", 1.0f);
+        }
+        else
+        {
+            currentPhase = gamePhase.Idle;
+            currentSpell = spellElement.Void;
+            headFireBig.GetComponent<VisualEffect>().Stop();
+            headFireSmall.GetComponent<VisualEffect>().Stop();
+        }
+    }
+
+    private void SelectingPhase()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRayObject.position, targetVector, out hit, Mathf.Infinity, transparentLayer))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                islandHighlightTransforms[i].GetComponent<MeshRenderer>().enabled = false;
+            }
+            hit.transform.GetComponent<MeshRenderer>().enabled = true;
+            currentTarget = hit.transform;
+        }
+    }
+
 
     private void FixedUpdate()
     {
@@ -223,14 +408,6 @@ public class MageControllerNew : MonoBehaviour
         rightHandController.rotation = Quaternion.LookRotation(mouseRayObject.right, mouseRayObject.forward);
     }
 
-    // Called by the base layer of Mage Animator Controller
-    //private void OnAnimatorIK()
-    //{
-    //    // Wand targeting IK
-    //    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0.4f);
-    //    animator.SetIKPosition(AvatarIKGoal.RightHand, targetTransform.position);
-    //}
-
     private void Damage()
     {
         if (currentHealth > 0)
@@ -241,3 +418,11 @@ public class MageControllerNew : MonoBehaviour
     }
 }
 
+
+// Called by the base layer of Mage Animator Controller
+//private void OnAnimatorIK()
+//{
+//    // Wand targeting IK
+//    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0.4f);
+//    animator.SetIKPosition(AvatarIKGoal.RightHand, targetTransform.position);
+//}
